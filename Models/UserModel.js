9dd481e8e6d52d0b2da9f8c1756de2033b727aac
sarvesh <dsarvesh92@gmail.com>
2025-03-29 21:50:10 +0530

    Setting up  bcrypt

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcrypt')
 const userSchema=new mongoose.Schema({
     username:{
         required:true,
@@ -21,7 +22,15 @@ const userSchema=new mongoose.Schema({
         required:true,
     }
 })
+userSchema.methods.hashpassword=async function(){
+         this.password = await bcrypt.hash(this.password, 8);
+}
+userSchema.methods.comparepassword=async function(password){
+   const istrue=  await bcrypt.compare(password,this.password);
+   return istrue;
+}
 const User=mongoose.model('Users',userSchema);
 module.exports={
     User,
+   
 }
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express= require("express");
+const bcrypt=require('bcrypt')
 const db=require("./UTILS/dbconnect");
 const { User } = require("./Models/UserModel");
 const app =express();
 app.use(express.json());
 require('dotenv').config({path:__dirname+"/.env"}); 
 app.get('/',function(req,res){
-    res.send("hello user welcome")
+    res.send("hello user welcome to initial page ")
 })
-app.get('/signup',async function(req,res){
+app.post('/signup',async function(req,res){
    try {
     const{username,password,email} = req?.body;
+    console.log(email)
             const searchdata=await User.find({email:email});
-            if(searchdata){
+            console.log(searchdata)
+            if(searchdata.length>0){
                 throw new Error("Email already exists please try another email")
             }
-       const data=await User.create({
+           
+       const data=await new User({
             username:username,
             password:password,
             email:email,
         })
+        await data.hashpassword();
+        await data.save();
+        // await data.save();
+      
+       
         console.log("data saved",data);
 res.status(201).json({message:"success"})
 
@@ -30,8 +39,28 @@ res.status(201).json({message:"success"})
    }
 })
 
-app.get('/signin',function(req,res){
-    res.send("hello user welcome")
+app.post('/signin',async function(req,res){
+    const {username,password}=req.body;
+    
+ try {
+       const isuser=await User.findOne({username:username});
+       console.log(password)
+       let pass = password.toString()
+       if(isuser){
+           const isvalid =await isuser.comparepassword(pass);
+           console.log(isvalid)
+       
+           if(isvalid){
+               res.json("signed in ")
+           }
+           else{
+            res.status(401).json("invalid credentials")
+           }
+        }
+ } catch (error) {
+    res.json({message :`invalid credentials->${error?.message}`})
+    
+ }
 })
 
 app.listen(3000,async function(req,res){
